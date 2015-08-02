3456b393b3b92815a12df04ca2667ef51b9d9322
ibillxia <ibillxia@gmail.com>
2015-08-02 21:17:42 +0800

    update ruby version and jekyll version

--- Gemfile
+++ Gemfile
@@ -1,18 +1,23 @@
 source "http://rubygems.org"
 
 group :development do
-  gem 'rake', '~> 0.9.6'
+  gem 'rake', '~> 10.0' # 0.9.6
   gem 'rack', '~> 1.4.1'
-  gem 'jekyll', '~> 0.12.1'  # 1.4.3
-  gem 'rdiscount', '~> 1.6.8'
-  gem 'pygments.rb', '~> 0.3.7'  # 0.5.0
+  gem 'jekyll', '~> 2.0'  # 1.4.3/0.12.1
+  gem 'octopress-hooks', '~> 2.2'
+  gem 'octopress-date-format', '~> 2.0'
+  gem 'jekyll-sitemap'
+  gem 'rdiscount', '~> 2.0' # 1.6.8
+  #gem 'pygments.rb', '~> 0.5.0'  # 0.3.7
   gem 'RedCloth', '~> 4.2.9'
-  gem 'haml', '~> 3.1.7'
-  gem 'compass', '~> 0.12.6'
-  gem 'rubypants', '~> 0.2.0'
+  gem 'haml', '~> 4.0' # 3.1.7
+  gem 'compass', '~> 1.0.1' # 0.12.6
+  #gem 'rubypants', '~> 0.2.0'
   gem 'rb-fsevent', '~> 0.9.4'
   gem 'stringex', '~> 1.4.0'
   gem 'liquid', '~> 2.5.5'
+  gem 'execjs', '~>2.5.2'
+  gem 'therubyracer', '~> 0.12.2'
 end
 
-gem 'sinatra', '~> 1.3.3'
+gem 'sinatra', '~> 1.4.2' # 1.3.3
--- plugins/backtick_code_block.rb
+++ plugins/backtick_code_block.rb
@@ -1,10 +1,9 @@
 require './plugins/pygments_code'
 
 module BacktickCodeBlock
-  include HighlightCode
   AllOptions = /([^\s]+)\s+(.+?)\s+(https?:\/\/\S+|\/\S+)\s*(.+)?/i
   LangCaption = /([^\s]+)\s*(.+)?/i
-  def render_code_block(input)
+  def self.render_code_block(input)
     @options = nil
     @caption = nil
     @lang = nil
@@ -17,29 +16,25 @@ module BacktickCodeBlock
       if @options =~ AllOptions
         @lang = $1
         @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
-        # @caption = "<div><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></div>"
       elsif @options =~ LangCaption
         @lang = $1
         @caption = "<figcaption><span>#{$2}</span></figcaption>"
-		# @caption = "<div><span>#{$2}</span></div>"
       end
 
       if str.match(/\A( {4}|\t)/)
         str = str.gsub(/^( {4}|\t)/, '')
       end
       if @lang.nil? || @lang == 'plain'
-        code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
+        code = HighlightCode::tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
         "<figure class='code'>#{@caption}#{code}</figure>"
-		# "<div class='code'>#{@caption}#{code}</div>"  
       else
         if @lang.include? "-raw"
           raw = "``` #{@options.sub('-raw', '')}\n"
           raw += str
           raw += "\n```\n"
         else
-          code = highlight(str, @lang)
+          code = HighlightCode::highlight(str, @lang)
           "<figure class='code'>#{@caption}#{code}</figure>"
-		  #"<div class='code'>#{@caption}#{code}</div>"  
         end
       end
     end
